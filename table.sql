1f1cebc677a57c3da5fc0835f21cfe6bacf11012
anoushkapradhan11 <pradhananoushka11@gmail.com>
2025-09-27 20:35:46 +0530

    Commit

---
+++ table.sql
@@ -0,0 +1,10 @@
+use startersql;
+create table users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    email varchar(100) UNIQUE NOT NULL,
+    gender enum('Male','Female','Other'),
+    date_of_birth date,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+select * FROM users;
